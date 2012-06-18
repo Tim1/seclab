1bf65db69cfdae2fb99ee8708a4d1dcdda6156c3
Tim Schmiedl <timschmiedl@gmx.net>
2012-06-18 10:58:04 +0200

    remove submit + license-Key

--- src/de/timweb/seclab/security/SubmitHigscore.java
+++ src/de/timweb/seclab/security/SubmitHigscore.java
@@ -13,30 +13,12 @@ public class SubmitHigscore {
 	private static final int MIN_POINTS = 3000;
 
 	public static void submit(int points) {
-		if (points < MIN_POINTS)
+		if (points < MIN_POINTS) {
 			JOptionPane.showMessageDialog(null, "Du musst mindestens " + MIN_POINTS + " erreichen, um einen Highscore zu submitten!",
-					"Not good enough", JOptionPane.INFORMATION_MESSAGE);
-		else {
-			try {
-				URL url = new URL("http://tareksaier.bplaced.net/notHP/gamescore/addscore.php?score="+points+"&key="+KeyReader.getSubmitKey());
-
-				URLConnection connection = url.openConnection();
-				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				
-				if(bufferedReader.readLine().contains("Roger")){
-					JOptionPane.showMessageDialog(null, "Glueckwunsch! Du bist mit " + points + " Punkten auf der Highscoreliste!",
-							"Higscore", JOptionPane.INFORMATION_MESSAGE);
-				}else{
-					JOptionPane.showMessageDialog(null, "Da ist leider was schief gelaufen",
-							"Error", JOptionPane.ERROR_MESSAGE);
-				}
-				
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
+					"Nicht gut genug", JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(null, "Glueckwunsch! Du bist mit " + points + " Punkten auf der Highscoreliste!", "Higscore",
+					JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
 }
